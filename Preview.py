b2c15bd2dfc5a6066b3dad3ec778285ced6a7c5a
BedeschiL <60644436+BedeschiL@users.noreply.github.com>
2023-03-03 08:03:22 +0100

    Update Preview.py
    JSON encode & decode are ok, next step is to implement  REST API

--- Preview.py
+++ Preview.py
@@ -4,6 +4,14 @@ from json import JSONEncoder
 class PreviewEncoder(JSONEncoder):
     def default(self, o):
          return o.__dict__
+    
+class PreviewDecoder(json.JSONDecoder):
+    def decode(self, json_str):
+        preview_dict = super().decode(json_str)
+        return Preview(preview_dict['title'], preview_dict['desc'], preview_dict['img'])
+
+def deserialize_preview(json_str):
+    return json.loads(json_str, cls=PreviewDecoder)
 
 class Preview:
     def __init__(self,_title,_img,_desc):
@@ -21,5 +29,10 @@ class Preview:
 
 
 p = Preview("LOUIS","https://media.giphy.com/media/8FNlmNPDTo2wE/giphy.gif","Blabla") 
+print(p)
 prevJSONData = json.dumps(p, indent=4, cls=PreviewEncoder)
 print(prevJSONData)
+
+prevJSONdecode=deserialize_preview(prevJSONData)
+print(type(prevJSONdecode))
+print(prevJSONdecode)
